c3d93290f131c2ccc5bff3e3368d2346d7042da8
HDMU <bonkelchen@gmail.com>
2014-06-13 19:37:16 +0200

    Update prepare_root.sh

--- at7500/scripts/prepare_root.sh
+++ at7500/scripts/prepare_root.sh
@@ -4,60 +4,42 @@ CURDIR=$1
 RELEASEDIR=$2
 TMPROOTDIR=$3
 TMPKERNELDIR=$4
-OWNLANG=$5
-OWNCOUNTRY=$6
 
-echo "Copying release image..."
+
 cp -a $RELEASEDIR/* $TMPROOTDIR
 
+
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/ar
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/bg
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/c*
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/da
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/el
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/es
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/et
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/f*
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/h*
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/i*
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/k*
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/l*
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/n*
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/p*
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/r*
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/s*
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/t*
+rm -rf $TMPROOTDIR/usr/local/share/enigma2/po/uk
 cd $TMPROOTDIR/dev/
 $TMPROOTDIR/etc/init.d/makedev start
-cd - > /dev/null
+cd -
 
-echo "Move kernel..."
 mv $TMPROOTDIR/boot/uImage $TMPKERNELDIR/uImage
 
-echo "Strip Root..."
-# Language support: remove everything but English, German and own language
-mv $TMPROOTDIR/usr/local/share/enigma2/po $TMPROOTDIR/usr/local/share/enigma2/po.old
-mkdir $TMPROOTDIR/usr/local/share/enigma2/po
-cp -r $TMPROOTDIR/usr/local/share/enigma2/po.old/en $TMPROOTDIR/usr/local/share/enigma2/po
-cp -r $TMPROOTDIR/usr/local/share/enigma2/po.old/de $TMPROOTDIR/usr/local/share/enigma2/po
-# Add own language if given
-if [[ ! "$OWNLANG" == "" ]]; then
-  cp -r $TMPROOTDIR/usr/local/share/enigma2/po.old/$OWNLANG $TMPROOTDIR/usr/local/share/enigma2/po
-fi
-sudo rm -rf $TMPROOTDIR/usr/local/share/enigma2/po.old
 
-#mv $TMPROOTDIR/usr/local/share/enigma2/countries $TMPROOTDIR/usr/local/share/enigma2/countries.old
-#mkdir $TMPROOTDIR/usr/local/share/enigma2/countries
-#cp -r $TMPROOTDIR/usr/local/share/enigma2/countries.old/missing.* $TMPROOTDIR/usr/local/share/enigma2/countries
-#cp -r $TMPROOTDIR/usr/local/share/enigma2/countries.old/en.* $TMPROOTDIR/usr/local/share/enigma2/countries
-#cp -r $TMPROOTDIR/usr/local/share/enigma2/countries.old/de.* $TMPROOTDIR/usr/local/share/enigma2/countries
-#if [[ ! "$OWNLANG" == "" ]]; then
-#  cp -r $TMPROOTDIR/usr/local/share/enigma2/countries.old/$OWNLANG.* $TMPROOTDIR/usr/local/share/enigma2/countries
-#fi
-#sudo rm -rf $TMPROOTDIR/usr/local/share/enigma2/countries.old
-
-# Update /usr/lib/enigma2/python/Components/Language.py
-# First remove all language lines from it
-#sed -i -e '/\t\tself.addLanguage(/d' $TMPROOTDIR/usr/lib/enigma2/python/Components/Language.py
-# Add en and ge
-#sed -i "s/country!/&\n\t\tself.addLanguage(\"Deutsch\",     \"de\", \"DE\")\n\t\tself.addLanguage(\"English\",     \"en\", \"EN\")/g" $TMPROOTDIR/usr/lib/enigma2/python/Components/Language.py
-# Add own language if given
-#if [[ ! "$OWNLANG" == "" ]]; then
- # sed -i 's/("English",     "en", "EN")/&\n\t\tself.addLanguage(\"Your own\",    \"'$OWNLANG'", \"'$OWNCOUNTRY'\")/g' $TMPROOTDIR/usr/lib/enigma2/python/Components/Language.py
-#fi
-#rm $TMPROOTDIR/usr/lib/enigma2/python/Components/Language.pyo
-# Compile Language.py
-#python -O -m py_compile $TMPROOTDIR/usr/lib/enigma2/python/Components/Language.py
+cp -rf $HOME/HDMU-BUILD/tools/boot/* $TMPROOTDIR/boot
+#sed -i "s/\/boot\/bootlogo.mvi/\/etc\/bootlogo.mvi/g" $TMPROOTDIR/etc/init.d/rcS
 
 
-# we need libav files
-#if [ -d $TMPROOTDIR/usr/lib/gstreamer-0.10 ]; then
-#  rm -f $TMPROOTDIR/usr/lib/libav*
-#fi
 
-#remove all .py-files
-find $TMPROOTDIR/usr/lib/ -name '*.pyc' -exec rm {} \;
-find $TMPROOTDIR/usr/lib/ -not -name 'mytest.py' -name '*.py' -exec rm -f {} \;
+#echo "/dev/mtdblock2	/boot	jffs2	defaults	0	0" >> $TMPROOTDIR/etc/fstab
+#echo "/dev/mtdblock4	/var	jffs2	defaults	0	0" >> $TMPROOTDIR/etc/fstab
+
+
