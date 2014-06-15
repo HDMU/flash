c2b6a96bb243023a87756f31b7b9fa09986eb221
HDMU <bonkelchen@gmail.com>
2014-06-15 16:29:44 +0200

    Update prepare_root.sh

--- ufs913/scripts/prepare_root.sh
+++ ufs913/scripts/prepare_root.sh
@@ -23,16 +23,14 @@ if [ ! -e $TMPROOTDIR/dev/mtd0 ]; then
 fi
 
 mv $TMPROOTDIR/boot/uImage $TMPKERNELDIR/uImage
-mv $TMPROOTDIR/lib/firmware/* $TMPFWDIR
 mv $TMPROOTDIR/boot/audio.elf $TMPFWDIR/
 mv $TMPROOTDIR/boot/video.elf $TMPFWDIR/
 rm -fr $TMPROOTDIR/boot
-mv $TMPROOTDIR/lib/firmware/* $TMPFWDIR
 
 if [ -e $TMPROOTDIR/var/etc/fstab ]; then
-	echo "/dev/mtdblock8	/lib/firmware	jffs2	defaults	0	0" >> $TMPROOTDIR/var/etc/fstab
+	echo "/dev/mtdblock8	/boot	jffs2	defaults	0	0" >> $TMPROOTDIR/var/etc/fstab
 	#echo "/dev/mtdblock10	/swap	jffs2	defaults	0	0" >> $TMPROOTDIR/var/etc/fstab
 else
-	echo "/dev/mtdblock8	/lib/firmware	jffs2	defaults	0	0" >> $TMPROOTDIR/etc/fstab
+	echo "/dev/mtdblock8	/boot	jffs2	defaults	0	0" >> $TMPROOTDIR/etc/fstab
 	#echo "/dev/mtdblock10	/swap	jffs2	defaults	0	0" >> $TMPROOTDIR/etc/fstab
 fi
